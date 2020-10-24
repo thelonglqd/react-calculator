73034d3d9a940f29002d235409c3f532687008b6
Long Nguyen The Phung <thelonglqd@gmail.com>
2020-10-24 19:37:06 +0700

    finish ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+
+import {
+  CgMathEqual,
+  CgMathDivide,
+  CgMathMinus,
+  CgMathPlus,
+  CgClose,
+} from "react-icons/cg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="display"></div>
+        <div className="btn-clear">CLEAR</div>
+        <div className="btn-equal">
+          <CgMathEqual size="30" color="white" />
+        </div>
+        <div className="btn-add">
+          <CgMathPlus size="30" color="white" />
+        </div>
+        <div className="btn-minus">
+          <CgMathMinus size="30" color="white" />
+        </div>
+        <div className="btn-multiply">
+          <CgClose size="30" color="white" />
+        </div>
+        <div className="btn-divide">
+          <CgMathDivide size="30" color="white" />
+        </div>
+        <div className="btn-number btn0">0</div>
+        <div className="btn-number btn1">1</div>
+        <div className="btn-number btn2">2</div>
+        <div className="btn-number btn3">3</div>
+        <div className="btn-number btn4">4</div>
+        <div className="btn-number btn5">5</div>
+        <div className="btn-number btn6">6</div>
+        <div className="btn-number btn7">7</div>
+        <div className="btn-number btn8">8</div>
+        <div className="btn-number btn9">9</div>
+      </div>
     </div>
   );
 }
